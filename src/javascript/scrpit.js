d81647a997ca095f994d9f18671159e0f518b096
jandirapedrosa <jandira.mpedrosa@gmail.com>
2025-02-18 21:23:30 +0000

    pequenas atualizações

--- src/javascript/scrpit.js
+++ src/javascript/scrpit.js
@@ -1,9 +1,16 @@
-document.querySelector('search').addEventListener('submit', (event) => {
+document.querySelector('#search').addEventListener('submit', (event) => {
   event.preventDefault();
 
-  const cityName = document.querySelector('city_name').value;
+  const cityName = document.querySelector('#city_name').value;
 
   if (!cityName) {
-    return alert('É preciso degitar uma cidade!');
+    return showAlert('É preciso digitar uma cidade!');
   }
+
+
 });
+
+// outra função - mostrar mensagem de alerta
+function showAlert(msg) {
+  document.querySelector('#alert').innerHTML = msg;
+}
