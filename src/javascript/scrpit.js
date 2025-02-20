fc99b386ba881f3a6bf50931c10328eaf474bf95
jandirapedrosa <jandira.mpedrosa@gmail.com>
2025-02-20 21:14:37 +0000

    continuação de js

--- src/javascript/scrpit.js
+++ src/javascript/scrpit.js
@@ -10,10 +10,32 @@ document.querySelector('#search').addEventListener('submit', (event) => {
 // quando tiver a minha chave, mudar na const apiUrl
   const apiKey = '';
 // depois é necessário colocar 'encodeURI' antes de city_key por causa dos acentos nas palavras
+// faltam mais coisas da API
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid={API key}`
 
+  // constante de resultados - vamos realizar a chamada da API usando o fetch
+  const results = await fetch(apoUrl);
+  // outra constante
+  const json = await results.json();
+
+// sobre os resultados
+  if (json.cod === 200) {
+    showInfo('FIQUEI AQUIIII!!!')
+  } else {
+    showAlert('Não foi possível localizar...')
+  }
 
 });
+
+// função para guardar toda a informação do if
+function showInfo(json) {
+
+}
+
+// função para exibir as informações
+function showAlert(msg) {
+  document.querySelector('#alert').innerHTML = msg;
+}
 
 // outra função - mostrar mensagem de alerta
 function showAlert(msg) {
